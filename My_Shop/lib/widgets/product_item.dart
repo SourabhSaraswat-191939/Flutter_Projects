1e78eca89c7a414ee0405abd869be8ec9097ba87
Sourabh Saraswat <saraswatsourabh5@gmail.com>
2021-06-09 00:57:28 +0530

    Updating My_Shop App
    Signed-off-by: Sourabh Saraswat <saraswatsourabh5@gmail.com>

--- My_Shop/lib/widgets/product_item.dart
+++ My_Shop/lib/widgets/product_item.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
+  // final String id;
+  // final String title;
   // final String description;
   // final double price;
-  final String imageUrl;
+  // final String imageUrl;
   // bool isFavorite;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context)
-                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         // header: ListTile(leading: CircleAvatar(child: Text('SA'),),),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+          leading: Consumer<Product>(
+            builder: (ctx, products, child) => IconButton(
+              icon: Icon(
+                  products.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                products.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           trailing: IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -39,7 +47,7 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
         ),
